5b858a6d630dc3a0a66020a7d551c5e15e959eec
wangbing587 <39249841+wangbing587@users.noreply.github.com>
2019-07-02 16:15:54 +0800

    类别型数据转数值型

--- class_mapping.py
+++ class_mapping.py
@@ -3,3 +3,10 @@ import pandas as pd
 
 class_mapping = {label:idx for idx,label in enumerate(np.unique(df['classlabel']))}
 df['classlabel'] = df['classlabel'].map(class_mapping)
+# 反转
+inv_class_mapping = {v: k for k, v in class_mapping.items()}
+df['classlabel'] = df['classlabel'].map(inv_class_mapping)
+
+from sklearn.preprocessing import LabelEncoder
+class_le = LabelEncoder()
+y = class_le.fit_transform(df['classlabel'].values)
