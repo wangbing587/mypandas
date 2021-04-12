c045a762845bb4791887fcdea0d8fcb53e25910c
wangbing587 <39249841+wangbing587@users.noreply.github.com>
2021-04-12 14:17:08 +0800

    Update showcolus.py

--- showcolus.py
+++ showcolus.py
@@ -1,4 +1,5 @@
-def showcolus(df):
-    return pd.DataFrame(np.array(df.columns.tolist() +
-             ['None{}'.format(i) for i in range(1, 11 - len(df.columns) % 10)]).reshape(-1,10),
-             columns=range(1, 11))
+def showcols(df):
+    cols = df.columns.tolist()
+    cols = cols + (10 - len(cols) % 10) % 10 * ['None']
+    cols = np.array(cols).reshape(-1, 10)
+    return pd.DataFrame(data=cols, columns=range(1, 11))
