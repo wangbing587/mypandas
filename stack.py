3474e5b4648041f41b5294ed41e93e3419d2a1d7
wangbing587 <39249841+wangbing587@users.noreply.github.com>
2021-04-15 19:40:28 +0800

    Update stack.py

--- stack.py
+++ stack.py
@@ -4,3 +4,9 @@ mapping1 = mapping.drop('isoform_id', axis=1).join(
 isoform_count = mapping1['gene_id'].value_counts().reset_index().rename(columns={'index': 'gene_id', 'gene_id': 'count'})
 
 mapping = pd.concat([mapping, mapping['gene_id'].str.split('|', expand=True).rename(columns={0: 'gene_name', 1: 'entrz_id'})], axis=1)
+
+
+obj = []
+for i in data1[col1[0]]:
+    lx = df1.loc[df1[col1[0]] == i, col2[1]].tolist()
+    obj.append(';'.join(str(j) for j in lx))
